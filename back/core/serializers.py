1492086cc3babd2c1320ede2910aa49668fd05b2
Ibrahim ADLANI <iadlani@deezer.com>
2024-04-04 13:50:13 +0200

    Changed the fibonacci function.

--- back/core/serializers.py
+++ back/core/serializers.py
@@ -42,12 +42,10 @@ class FibonacciResultSerializer(serializers.ModelSerializer):
         """
         Fibbonaci function to calculate the nth number in the sequence.
         """
-        if n == 0: return 0
-        elif n == 1: return 1
         a, b = 0, 1
-        for _ in range(2, n + 1):
+        for _ in range(n):
             a, b = b, a + b
-        return b
+        return a
 
 
 class UserSerializer(serializers.ModelSerializer):
